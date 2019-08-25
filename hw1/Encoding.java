00f4355c480e2fd198e7c539d1f88723ff0f9b73
shrutipanchal7699 <54422924+shrutipanchal7699@users.noreply.github.com>
2019-08-25 03:02:36 -0700

    Update Encoding.java

--- hw1/Encoding.java
+++ hw1/Encoding.java
@@ -1,1 +1,34 @@
+import java.util.*;
 
+public class Encoding
+{
+   public static Set<String> morseCodes(int m, int n)
+   {
+      Set<String> result = new TreeSet<>();
+      if(m==0 && n==0)
+       result.add("");
+    
+      else if (m==0 && n>=0)
+      {
+         for (String str : morseCodes(m,n-1))
+        	  result.add(str+"-");
+      }
+      else if (m>=0 && n==0 )
+      {
+    	  for (String str : morseCodes(m-1,n))
+    	   result.add(str+".");
+      }
+      else
+      {
+    	  for (String str : morseCodes(m-1,n))
+          {
+        	  result.add(str+".");
+          }
+          for (String str : morseCodes(m,n-1))
+          {
+        	  result.add(str+"-");
+          }
+      }
+     return result;
+   }
+}
