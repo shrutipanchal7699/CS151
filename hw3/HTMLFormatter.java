8502762c04db670f5bdc98d8423c47607500cf74
shrutipanchal7699 <54422924+shrutipanchal7699@users.noreply.github.com>
2019-10-07 19:44:43 -0700

    Update HTMLFormatter.java

--- hw3/HTMLFormatter.java
+++ hw3/HTMLFormatter.java
@@ -1,24 +1,23 @@
-import javax.swing.text.html.HTML;
 
 public class HTMLFormatter implements InvoiceFormatter
 {
 	public String formatHeader()
    {
 		total = 0;
-	      return "     I N V O I C E\n\n\n";
-		//return "<html><body><p>Hi How are u</p></body></html>";
+	      return " <b><h1 style='color:red';align= 'center'>"+"I N V O I C E" + "</h1></b><br><br>";
+		
    }
 
    public String formatLineItem(LineItem item)
    {
 	   total += item.getPrice()*item.getQuantity();
-      return (String.format("%s: $%.2f\n",item.toString(),item.getPrice()));
-   }
+      return "<li><p style = 'color: black'>" + item.toString() + " x" + item.getQuantity() + 
+				"<p style = 'color: green'> $ " + item.getPrice() + "</p></li><br><br><br>"; }
 
    public String formatFooter()
    {
-      return (String.format("\n\nTOTAL DUE: $%.2f\n", total));
-   }
+       return "<b><u><h3 style = 'color: black' ; align = 'center'>"
+				+ "TOTAL DUE: " + total + "</h3></u></b>"; }
 
    private double total;
    private InvoiceTester IT = new InvoiceTester();
